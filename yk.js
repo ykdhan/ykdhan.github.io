e23f440300c2e86420e64839ce26f4cc0d58a7cd
David <ykdhan@gmail.com>
2021-09-28 23:12:44 +0900

    full

--- yk.js
+++ yk.js
@@ -1,17 +1,17 @@
 window.onload = () => {
     const prop = {
-        number: window.innerWidth < 767 ? 55 : 80,
-        size: 5,
+        number: window.innerWidth < 767 ? 90 : 150,
+        size: window.innerWidth < 767 ? 5 : 7,
         speed: window.innerWidth < 767 ? 0.1 : 0.2,
         color: 255,
-        line: window.innerWidth < 767 ? 40 : 50,
-        w: $('.cvs').width(),
-        h: $('.cvs').height(),
+        line: window.innerWidth < 767 ? 50 : 70,
+        w: window.innerWidth,
+        h: window.innerHeight,
     }
 
     const chain = (p5) => {
         class Block {
-            constructor(x, y, dx, dy) {
+            constructor() {
                 this.x = Math.floor(Math.random() * prop.w)
                 this.y = Math.floor(Math.random() * prop.h)
                 this.dx = (prop.speed + Math.random()) * (Math.random() < 0.5 ? -1 : 1)
@@ -38,67 +38,62 @@ window.onload = () => {
         const blocks = new Array(prop.number)
 
         p5.setup = () => {
-            console.log(prop.w, prop.h)
             p5.createCanvas(prop.w * 1.2, prop.h * 1.2)
-            $('.title').css('transform', 'translate(-50%, calc(-50% - 30px))')
-            $('.github').css('transform', 'translate(-50%, calc(-50% + 30px))')
-            // p5.noStroke()
-            // p5.rectMode(p5.CENTER)
+            p5.noStroke()
+            p5.rectMode(p5.CENTER)
 
-            // for (let i = 0; i < blocks.length; i ++) {
-            //     blocks[i] = new Block(i, i)
-            // }
+            for (let i = 0; i < blocks.length; i ++) {
+                blocks[i] = new Block()
+            }
         }
 
         p5.draw = () => {
-            !$('html').hasClass('chain') && $('main canvas').length && $('html').addClass('chain')
             p5.clear()
             
-            // blocks.forEach((block, index) => {
-            //     if (block.x + block.dx > prop.w * 1.2 || block.x + block.dx < 0) {
-            //         block.changeDirX();
-            //     }
-            //     if (block.y + block.dy > prop.h * 1.2 || block.y + block.dy < 0) {
-            //         block.changeDirY();
-            //     }
+            blocks.forEach((block, index) => {
+                if (block.x + block.dx > prop.w * 1.2 || block.x + block.dx < 0) {
+                    block.changeDirX();
+                }
+                if (block.y + block.dy > prop.h * 1.2 || block.y + block.dy < 0) {
+                    block.changeDirY();
+                }
 
-            //     block.move()
+                block.move()
 
-            //     blocks.forEach((block2, index2) => {
-            //         if (index != index2 && 
-            //             Math.abs(block.x - block2.x) < prop.line && 
-            //             Math.abs(block.y - block2.y) < prop.line) {
-            //             const l = {
-            //                 from: { x: block.x, y: block.y },
-            //                 to: { x: block2.x, y: block2.y },
-            //             }
+                blocks.forEach((block2, index2) => {
+                    if (index != index2 && 
+                        Math.abs(block.x - block2.x) < prop.line && 
+                        Math.abs(block.y - block2.y) < prop.line) {
+                        const l = {
+                            from: { x: block.x, y: block.y },
+                            to: { x: block2.x, y: block2.y },
+                        }
 
-            //             p5.push()
-            //             p5.stroke(prop.color);
-            //             p5.strokeWeight(window.innerWidth < 767 ? 0.5 : 1)
-            //             p5.line(l.from.x, l.from.y, l.to.x, l.to.y);
-            //             p5.pop()
-            //         }
-            //     })
+                        p5.push()
+                        p5.stroke(prop.color);
+                        p5.strokeWeight(window.innerWidth < 767 ? 0.5 : 1)
+                        p5.line(l.from.x, l.from.y, l.to.x, l.to.y);
+                        p5.pop()
+                    }
+                })
                     
-            //     p5.push()
-            //     p5.fill(prop.color)
-            //     // p5.rotate(Math.PI * block.rotate + p5.frameCount)
-            //     p5.rect(block.x, block.y, block.size, block.size)
-            //     p5.pop()
-            // })
+                p5.push()
+                p5.fill(prop.color)
+                // p5.rotate(Math.PI * block.rotate + p5.frameCount)
+                p5.rect(block.x, block.y, block.size, block.size)
+                p5.pop()
+            })
         }
 
         p5.windowResized = () => {
-            prop.number = window.innerWidth < 767 ? 55 : 80
+            prop.number = window.innerWidth < 767 ? 90 : 150
+            prop.size = window.innerWidth < 767 ? 5 : 7
             prop.speed = window.innerWidth < 767 ? 0.1 : 0.2
-            prop.line = window.innerWidth < 767 ? 40 : 50
-            prop.w = $('.cvs').width()
-            prop.h = $('.cvs').height()
+            prop.line = window.innerWidth < 767 ? 50 : 70
+            prop.w = window.innerWidth
+            prop.h = window.innerHeight
 
-            // p5.resizeCanvas(prop.w * 1.2, prop.h * 1.2)
-            // $('.title').css('transform', 'translate(-50%, calc(-50% - ' + prop.h / 1.5 + 'px))')
-            // $('.github').css('transform', 'translate(-50%, calc(-50% + ' + prop.h / 1.5 + 'px))')
+            p5.resizeCanvas(prop.w * 1.2, prop.h * 1.2)
         }
     }
 
